ee01dc025ef52be947fa59991eff5d7835a57872
123456466 <sunji7378@gmail.com>
2025-03-22 19:42:58 +0900

    feat : chldren 타입 확실하게 정의

--- src/components/components/MonthBox.tsx
+++ src/components/components/MonthBox.tsx
@@ -1,4 +1,10 @@
-const MonthBox = ({ chldren }: any) => {
+import { ReactNode } from "react";
+
+interface MonthBoxProps {
+  chldren: ReactNode;
+}
+
+const MonthBox = ({ chldren }: MonthBoxProps) => {
   return (
     <div className="cursor-pointer outline-blue-300 outline-1 rounded-md w-40 h-18 m-5 text-4xl text-indigo-900 flex justify-center items-center hover:bg-sky-200/75">
       {chldren}
