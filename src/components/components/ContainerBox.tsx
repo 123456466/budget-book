a19c363c7cbeea392ed33cd7cecf17d1d3e62ce1
123456466 <sunji7378@gmail.com>
2025-03-22 20:26:36 +0900

    fix :  스타일링 오류 고침

--- src/components/components/ContainerBox.tsx
+++ src/components/components/ContainerBox.tsx
@@ -2,14 +2,11 @@ import { ReactNode } from "react";
 
 interface ContainerBoxProps {
   children: ReactNode;
-  mtNum: string;
 }
 
-const ContainerBox = ({ children, mtNum }: ContainerBoxProps) => {
+const ContainerBox = ({ children }: ContainerBoxProps) => {
   return (
-    <div
-      className={`bg-white p-5 rounded-md outline-blue-500 outline-2 mt-${mtNum}`}
-    >
+    <div className={`bg-white p-5 rounded-md outline-blue-500 outline-2 mt-10`}>
       {children}
     </div>
   );
